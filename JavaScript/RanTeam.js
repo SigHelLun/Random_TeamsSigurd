159808d41c03b38fd0b16943be6bcc5253115d17
SigHelLun <116567255+SigHelLun@users.noreply.github.com>
2023-10-16 13:43:09 +0200

    new update
    it ain't working

--- JavaScript/RanTeam.js
+++ JavaScript/RanTeam.js
@@ -1,5 +1,19 @@
-function saveList() 
+function runList() 
 {
     const groupText = document.getElementById('group').value;
     const spliter = groupText.split(",")
+
+}
+function runInt()
+{
+    const amountInt = document.getElementById('amount').value;
+    const groupAmount = parseInt(amountInt);
+    let timesHell = 0
+    const newInt = document.createElement('amount')
+
+    while (timesHell < groupAmount)
+    {
+        newInt;
+        timesHell++;
+    }
 }
